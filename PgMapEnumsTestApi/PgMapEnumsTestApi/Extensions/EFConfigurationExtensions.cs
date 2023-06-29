f26adc9d03c8b054ea4ec8da5771589ce35f95ab
Ivan Martyshin <Ivan Martyshin>
2023-06-29 19:28:07 +0400

    Message

--- PgMapEnumsTestApi/PgMapEnumsTestApi/Extensions/EFConfigurationExtensions.cs
+++ PgMapEnumsTestApi/PgMapEnumsTestApi/Extensions/EFConfigurationExtensions.cs
@@ -9,14 +9,20 @@ namespace PgMapEnumsTestApi.Ioc
 	{
 		public static IServiceCollection AddMyDbContext(this IServiceCollection services, IConfiguration configuration)
 		{
-			//services.AddNpgsqlDataSource(configuration["Settings:ConnectionString"], builder =>
-			//{
-			//    builder?.MapEnum<OneTimePasswordStatus>();
-			//    builder?.MapEnum<UserRegistrationStatus>();
-			//    builder?.MapEnum<UserRoles>();
-			//});
+            /*
+			 * It seems that I did everything according to the instructions, at the bottom there are two options for how I tried to run it. But none of them work. 
+			 * I get a message saying that it is impossible to cast to int32 or else, a message saying "Can't cast database type text to UserRoles"
+			 * I don't understand what I'm doing wrong anymore :(
+			 */
 
-			var dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration["Settings:ConnectionString"]);
+            //services.AddNpgsqlDataSource(configuration["Settings:ConnectionString"], builder =>
+            //{
+            //    builder?.MapEnum<OneTimePasswordStatus>();
+            //    builder?.MapEnum<UserRegistrationStatus>();
+            //    builder?.MapEnum<UserRoles>();
+            //});
+
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration["Settings:ConnectionString"]);
 
 			dataSourceBuilder.MapEnum<UserRoles>();
 			
